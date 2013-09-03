9dc62070b8ed01dfb8d3bb2e5c96cf3fc07fd293
Elliot Saba <staticfloat@gmail.com>
2013-09-03 13:01:25 -0400

    Add in Homebrew provider for OSX

--- deps/build.jl
+++ deps/build.jl
@@ -7,4 +7,11 @@ zmq = library_dependency("zmq", aliases = ["libzmq"])
 provides(Sources,URI("http://download.zeromq.org/zeromq-3.2.3.tar.gz"),zmq)
 provides(BuildProcess,Autotools(libtarget = "src/.libs/libzmq."*BinDeps.shlib_ext),zmq)
 
+@osx_only begin
+    if Pkg.installed("Homebrew") === nothing
+        error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")  end
+    using Homebrew
+    provides( Homebrew.HB, "zeromq", zmq, os = :Darwin )
+end
+
 @BinDeps.install
