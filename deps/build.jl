5b8457f865dbcc1dbf523d0b14132a75375b701d
Steven G. Johnson <stevenj@mit.edu>
2014-01-29 11:43:44 -0500

    update source package 3.2.3 -> 3.2.4 (fix #43)

--- deps/build.jl
+++ deps/build.jl
@@ -4,7 +4,7 @@ using BinDeps
 
 zmq = library_dependency("zmq", aliases = ["libzmq"])
 
-provides(Sources,URI("http://download.zeromq.org/zeromq-3.2.3.tar.gz"),zmq)
+provides(Sources,URI("http://download.zeromq.org/zeromq-3.2.4.tar.gz"),zmq)
 provides(BuildProcess,Autotools(libtarget = "src/.libs/libzmq."*BinDeps.shlib_ext),zmq)
 
 @windows_only begin
