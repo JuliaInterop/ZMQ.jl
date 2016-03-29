792c005ede9e6538973d6b1741752bbcfcc91396
Tony Kelman <tony@kelman.net>
2016-03-28 18:06:41 -0700

    add my OBS branch as a winrpm source

--- deps/build.jl
+++ deps/build.jl
@@ -10,6 +10,8 @@ provides(BuildProcess,Autotools(libtarget = "src/.libs/libzmq."*BinDeps.shlib_ex
 
 if is_windows()
     using WinRPM
+    unshift!(WinRPM.sources, "http://download.opensuse.org/repositories/home:/kelman:/branches:/windows:/mingw:/win64/openSUSE_13.2")
+    WinRPM.update()
     provides(WinRPM.RPM, "zeromq", [zmq], os = :Windows)
 end
 
