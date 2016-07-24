b490405ad564032dbbd7c833009e6beebe63eff7
WooKyoung Noh <wookay.noh@gmail.com>
2016-07-24 22:26:45 +0900

    compat unsafe_string to deprecate bytestring, compat os_only macros

--- deps/build.jl
+++ deps/build.jl
@@ -8,12 +8,12 @@ zmq = library_dependency("zmq", aliases = ["libzmq"])
 provides(Sources,URI("https://archive.org/download/zeromq_3.2.4/zeromq-3.2.4.tar.gz"),zmq)
 provides(BuildProcess,Autotools(libtarget = "src/.libs/libzmq."*BinDeps.shlib_ext),zmq)
 
-@windows_only begin
+if is_windows()
     using WinRPM
     provides(WinRPM.RPM, "zeromq", [zmq], os = :Windows)
 end
 
-@osx_only begin
+if is_apple()
     if Pkg.installed("Homebrew") === nothing
         error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")  end
     using Homebrew
