cd4473b9c2ecf1b46e8b136e4b788b3bfc618014
JamesWrigley <james@puiterwijk.org>
2024-05-31 13:34:07 +0200

    fixup! Generate bindings for zmq.h using Clang.jl

--- src/msg_bindings.jl
+++ src/msg_bindings.jl
@@ -2,58 +2,58 @@ import ZeroMQ_jll: libzmq
 import .lib: zmq_free_fn
 
 function lib.zmq_msg_init(msg_::Message)
-    @ccall libzmq.zmq_msg_init(msg_::Ref{Message})::Cint
+    ccall((:zmq_msg_init, libzmq), Cint, (Ref{Message},), msg_)
 end
 
 function lib.zmq_msg_init_size(msg_::Message, size_)
-    @ccall libzmq.zmq_msg_init_size(msg_::Ref{Message}, size_::Csize_t)::Cint
+    ccall((:zmq_msg_init_size, libzmq), Cint, (Ref{Message}, Csize_t), msg_, size_)
 end
 
 function lib.zmq_msg_init_data(msg_::Message, data_, size_, ffn_, hint_)
-    @ccall libzmq.zmq_msg_init_data(msg_::Ref{Message}, data_::Ptr{Cvoid}, size_::Csize_t, ffn_::Ptr{zmq_free_fn}, hint_::Ptr{Cvoid})::Cint
+    ccall((:zmq_msg_init_data, libzmq), Cint, (Ref{Message}, Ptr{Cvoid}, Csize_t, Ptr{zmq_free_fn}, Ptr{Cvoid}), msg_, data_, size_, ffn_, hint_)
 end
 
 function lib.zmq_msg_send(msg_::Message, s_, flags_)
-    @ccall libzmq.zmq_msg_send(msg_::Ref{Message}, s_::Ptr{Cvoid}, flags_::Cint)::Cint
+    ccall((:zmq_msg_send, libzmq), Cint, (Ref{Message}, Ptr{Cvoid}, Cint), msg_, s_, flags_)
 end
 
 function lib.zmq_msg_recv(msg_::Message, s_, flags_)
-    @ccall libzmq.zmq_msg_recv(msg_::Ref{Message}, s_::Ptr{Cvoid}, flags_::Cint)::Cint
+    ccall((:zmq_msg_recv, libzmq), Cint, (Ref{Message}, Ptr{Cvoid}, Cint), msg_, s_, flags_)
 end
 
 function lib.zmq_msg_close(msg_::Message)
-    @ccall libzmq.zmq_msg_close(msg_::Ref{Message})::Cint
+    ccall((:zmq_msg_close, libzmq), Cint, (Ref{Message},), msg_)
 end
 
 function lib.zmq_msg_move(dest_::Message, src_)
-    @ccall libzmq.zmq_msg_move(dest_::Ref{Message}, src_::Ref{Message})::Cint
+    ccall((:zmq_msg_move, libzmq), Cint, (Ref{Message}, Ref{Message}), dest_, src_)
 end
 
 function lib.zmq_msg_copy(dest_::Message, src_)
-    @ccall libzmq.zmq_msg_copy(dest_::Ref{Message}, src_::Ref{Message})::Cint
+    ccall((:zmq_msg_copy, libzmq), Cint, (Ref{Message}, Ref{Message}), dest_, src_)
 end
 
 function lib.zmq_msg_data(msg_::Message)
-    @ccall libzmq.zmq_msg_data(msg_::Ref{Message})::Ptr{Cvoid}
+    ccall((:zmq_msg_data, libzmq), Ptr{Cvoid}, (Ref{Message},), msg_)
 end
 
 function lib.zmq_msg_size(msg_::Message)
-    @ccall libzmq.zmq_msg_size(msg_::Ref{Message})::Csize_t
+    ccall((:zmq_msg_size, libzmq), Csize_t, (Ref{Message},), msg_)
 end
 
 function lib.zmq_msg_more(msg_::Message)
-    @ccall libzmq.zmq_msg_more(msg_::Ref{Message})::Cint
+    ccall((:zmq_msg_more, libzmq), Cint, (Ref{Message},), msg_)
 end
 
 function lib.zmq_msg_get(msg_::Message, property_)
-    @ccall libzmq.zmq_msg_get(msg_::Ref{Message}, property_::Cint)::Cint
+    ccall((:zmq_msg_get, libzmq), Cint, (Ref{Message}, Cint), msg_, property_)
 end
 
 function lib.zmq_msg_set(msg_::Message, property_, optval_)
-    @ccall libzmq.zmq_msg_set(msg_::Ref{Message}, property_::Cint, optval_::Cint)::Cint
+    ccall((:zmq_msg_set, libzmq), Cint, (Ref{Message}, Cint, Cint), msg_, property_, optval_)
 end
 
 function lib.zmq_msg_gets(msg_::Message, property_)
-    @ccall libzmq.zmq_msg_gets(msg_::Ref{Message}, property_::Ptr{Cchar})::Ptr{Cchar}
+    ccall((:zmq_msg_gets, libzmq), Ptr{Cchar}, (Ref{Message}, Ptr{Cchar}), msg_, property_)
 end
 
